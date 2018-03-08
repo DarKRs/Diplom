855b6d9be2ad86e9790614e03166854113dbab90
DarKRs <centralstreet_24@mail.ru>
2018-03-08 13:55:10 +0700

    Работа с текстом
    Работа с текстом и загрузка словарей

--- Diplom/Form1.Designer.cs
+++ Diplom/Form1.Designer.cs
@@ -29,12 +29,13 @@
         private void InitializeComponent()
         {
             this.SourseText = new System.Windows.Forms.RichTextBox();
-            this.button1 = new System.Windows.Forms.Button();
+            this.OpenText = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.HELPToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.AboutAutorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.OpenSourseText = new System.Windows.Forms.OpenFileDialog();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -47,14 +48,15 @@
             this.SourseText.TabIndex = 0;
             this.SourseText.Text = "";
             // 
-            // button1
+            // OpenText
             // 
-            this.button1.Location = new System.Drawing.Point(295, 45);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(101, 33);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "Открыть текст";
-            this.button1.UseVisualStyleBackColor = true;
+            this.OpenText.Location = new System.Drawing.Point(295, 45);
+            this.OpenText.Name = "OpenText";
+            this.OpenText.Size = new System.Drawing.Size(101, 33);
+            this.OpenText.TabIndex = 1;
+            this.OpenText.Text = "Открыть текст";
+            this.OpenText.UseVisualStyleBackColor = true;
+            this.OpenText.Click += new System.EventHandler(this.OpenText_Click);
             // 
             // button2
             // 
@@ -64,6 +66,7 @@
             this.button2.TabIndex = 2;
             this.button2.Text = "Найти стихи >>";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // richTextBox1
             // 
@@ -94,8 +97,12 @@
             // AboutAutorToolStripMenuItem
             // 
             this.AboutAutorToolStripMenuItem.Name = "AboutAutorToolStripMenuItem";
-            this.AboutAutorToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.AboutAutorToolStripMenuItem.Size = new System.Drawing.Size(130, 22);
             this.AboutAutorToolStripMenuItem.Text = "Об авторе";
+            // 
+            // OpenSourseText
+            // 
+            this.OpenSourseText.FileName = "openFileDialog1";
             // 
             // SearchVerse
             // 
@@ -104,7 +111,7 @@
             this.ClientSize = new System.Drawing.Size(714, 369);
             this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.OpenText);
             this.Controls.Add(this.SourseText);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
@@ -120,12 +127,13 @@
         #endregion
 
         private System.Windows.Forms.RichTextBox SourseText;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button OpenText;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.RichTextBox richTextBox1;
         private System.Windows.Forms.MenuStrip menuStrip1;
         private System.Windows.Forms.ToolStripMenuItem HELPToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem AboutAutorToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog OpenSourseText;
     }
 }
 
--- Diplom/Form1.cs
+++ Diplom/Form1.cs
@@ -1,25 +1,86 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Text.RegularExpressions;
 
 namespace Diplom
 {
     public partial class SearchVerse : Form
     {
+        string[] Words; //Главный массив исходных слов (уже разбитых)
+        string[] WordsAccent; //Главный массив слов ударений (Формуриуется из БД при иницилизации)
         public SearchVerse()
         {
             InitializeComponent();
+            Initialization();
         }
 
         void Initialization()
         {
+            string path = System.IO.Directory.GetCurrentDirectory() + @"\BD\";
+            /////////////////////Загрузка словаря ударений////////////////////////
+            string A = "Slovar_udareny.txt";
+            FileStream fs = new FileStream(path + A, FileMode.Open);
+            StreamReader reader = new StreamReader(fs, System.Text.Encoding.Default);
+            string file = reader.ReadToEnd();
+            file = ReplaceNR(file);
+            WordsAccent = file.Split(' ');
+            WordsAccent = WordsAccent.Where(x => x != "").ToArray();
+            /////////////////////////////////////////////////////
+            reader.Close();
+            fs.Close();
+            /////////////////////Загрузка словаря слогов////////////////////////
+            string A = "Slovar_udareny.txt";
+            FileStream fs = new FileStream(path + A, FileMode.Open);
+            StreamReader reader = new StreamReader(fs, System.Text.Encoding.Default);
+            string file = reader.ReadToEnd();
+            file = ReplaceNR(file);
+            WordsAccent = file.Split(' ');
+            WordsAccent = WordsAccent.Where(x => x != "").ToArray();
 
         }
+
+        private void button2_Click(object sender, EventArgs e)//Найти стихи >>
+        {
+            string Sourse;
+            Sourse = SourseText.Text;
+            Words = Sourse.Split(' ');
+            ////////////////////////////
+
+        }
+
+        private void OpenText_Click(object sender, EventArgs e)
+        {
+            OpenSourseText.Filter = "Text(*.txt)|*.txt";
+            OpenSourseText.ShowDialog();
+            string path = OpenSourseText.FileName;
+            if (OpenSourseText.FileName != "") //если в окне была нажата кнопка "ОК"
+            {
+                FileStream fs = new FileStream(path, FileMode.Open);
+                StreamReader reader = new StreamReader(fs, System.Text.Encoding.Default);
+                string file = reader.ReadToEnd();
+                //////////////////////////////
+                file = ReplaceNR(file);
+                SourseText.Text = file;
+            }
+        }
+
+        private string ReplaceNR(string file)//Убирание новой строки и кареток \n & \r
+        {
+            var ReN = new Regex("\n");
+            var ReR = new Regex("\r");
+            file = ReN.Replace(file, "");
+            file = ReR.Replace(file, "");
+            return file;
+        }
+
     }
 }
