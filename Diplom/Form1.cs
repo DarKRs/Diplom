53053a098d2d714555dfd00b56ab4d6598fa82ad
DarKRs <centralstreet_24@mail.ru>
2018-05-08 15:32:45 +0700

    Переработан поиск ударений
    Полностью был переработан поиск ударений теперь используется три варианта зменения слова:
    -Убрать 1 букву в конце
    -Убрать по окончанию
    -Убрать несколько букв в конце
    Помогло найти многие ударения.
    Так же исправленны некоторые баги и добавлено множество комментариев

--- Diplom/Form1.cs
+++ Diplom/Form1.cs
@@ -1,13 +1,8 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Text.RegularExpressions;
 
@@ -18,9 +13,11 @@ namespace Diplom
         int qlex = 0; //Счётчик для LEXGROUP
         string[] Words; //Главный массив исходных слов (уже разбитых)
         string[] WordsAccent; //Главный массив слов ударений (Формуриуется из БД при иницилизации)
-        LEXGROUP[] lexgroup;
-        Dictionary<string, word> WordDictionary = new Dictionary<string, word>();
-        Dictionary<string, string> WordAccentDictionary = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase);//Словарь слов с ударениями(Формуриуется из БД при иницилизации)
+        LEXGROUP[] lexgroup; //Массив LEXGROUP хранящий в себе начало слов и их возможные окончания
+        Dictionary<string, word> WordDictionary = new Dictionary<string, word>(); //Словарь объектов класса word. В котором хранятся ударения, слоги и т.д.
+        Dictionary<string, string> WordAccentDictionary = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase);//Словарь ударений(Формуриуется из БД при иницилизации)
+
+
         public SearchVerse()
         {
             InitializeComponent();
@@ -29,7 +26,7 @@ namespace Diplom
 
         void Initialization()
         {
-            var re = new Regex("'");
+            var re = new Regex("'"); //Регулярное выражение ударения
             string path = System.IO.Directory.GetCurrentDirectory() + @"\BD\";
             /////////////////////Загрузка словаря ударений////////////////////////
             string A = "Slovar_udareny.txt";
@@ -41,22 +38,23 @@ namespace Diplom
             WordsAccent = file.Split(new char[] { ' ', '\n' , '\r' , '!' }, StringSplitOptions.RemoveEmptyEntries);
             for(int i = 0; i < WordsAccent.Length-1; i++)
             {
-                if (WordAccentDictionary.ContainsKey(WordsAccent[i]))
+                if (WordAccentDictionary.ContainsKey(WordsAccent[i])) //Если уже есть такое слово в словаре то пропускаем
                 {
                     i++;
                     continue;
                 }
-                if(WordAccentDictionary.ContainsKey(WordsAccent[i] + " " + WordsAccent[i + 1]))
+                if(WordAccentDictionary.ContainsKey(WordsAccent[i] + " " + WordsAccent[i + 1])) //Если уже есть такое словочетание
                 {
                     i += 2;
                     continue;
                 }
-                if (WordsAccent[i] == WordsAccent[i + 1] && !WordAccentDictionary.ContainsKey(WordsAccent[i]))
+                if (WordsAccent[i] == WordsAccent[i + 1] && !WordAccentDictionary.ContainsKey(WordsAccent[i])) 
                 {
                     //////////Слово без ударения/////////////
                     WordAccentDictionary.Add(WordsAccent[i], "");
                     i++;
                 }
+                //Если следуйщее слово содержит знак ударения И это слово совпадает со следуйщим если убрать знак ударения И его нет в словаре
                 else if (WordsAccent[i + 1].Contains('\'') && WordsAccent[i] == re.Replace(WordsAccent[i + 1],"") && !WordAccentDictionary.ContainsKey(WordsAccent[i]))
                 {
                     WordAccentDictionary.Add(WordsAccent[i], WordsAccent[i + 1]);
@@ -73,7 +71,7 @@ namespace Diplom
             /////////////////////////////////////////////////////
             reader.Close();
             fs.Close();
-            /////////////////////////////////////////////////////Чтение всех файлов//////////////////
+            /////////////////////////////Чтение всех файлов для морфологического словаря//////////////////
             path = System.IO.Directory.GetCurrentDirectory() + @"\BD\LexGroup\GLAG\";
             int leng = new DirectoryInfo(path).GetFiles().Length; //Получает кол-во файлов в папке. 
             lexgroup = new LEXGROUP[854]; //Создание объектов LEXGROUP для удобной работы с ними
@@ -87,6 +85,11 @@ namespace Diplom
             }
         }
 
+        /// <summary>
+        /// Читает все файлы LEXGROUP из папки (path) и записывает их в массив lexgroup
+        /// </summary>
+        /// <param name="path">Путь до папки с файлами LEXGROUP</param>
+        /// <param name="leng">Кол-во файлов в папке</param>
         private void ReadAllFiles(string path, int leng)
         {
             FileStream fs;
@@ -108,18 +111,17 @@ namespace Diplom
                     string[] textOfFile = file2.Split(new char[] { '\n', '\r', }, StringSplitOptions.RemoveEmptyEntries);
                     for (int i = 0; i < textOfFile.Length; i++)
                     {
-                        if (textOfFile[i].Contains('{'))
+                        if (textOfFile[i].Contains('{'))//Если строка содержит { значит это новый набор слов и окончаний для них
                         {
-                            
                             lexgroup[qlex] = new LEXGROUP();
-                            lexgroup[qlex].ends = Skobki(textOfFile[i]);
+                            lexgroup[qlex].ends = OpeningBrackets(textOfFile[i]); // С помощью функции Openingbrackets раскрываем скобки и записываем массив окончаний
                             qlex++;
                             continue;
                         }
                         lexgroup[qlex - 1].words += textOfFile[i] + ",";
                     }
                 }
-                catch { continue; }
+                catch { continue; } //Если по какой-то причине не удалось открыть файл продолжаем чтение других
             }
         }
 
@@ -128,42 +130,40 @@ namespace Diplom
         /// </summary>
         /// <param name="s">Строка со скобками</param>
         /// <returns>Возвращает массив готовых окончаний (открытие скобок сделано)</returns>
-        private string[] Skobki(string s)
+        private string[] OpeningBrackets(string s)
         {
             int o = 0; //Счётчик для вставки раскрытых слов в конечный массив
-            //Избавляемся от лишнего текста в конце. (+1 чтобы осталась скобка)
+            //Избавляемся от лишнего текста в конце. 
             string text = s.Remove(s.LastIndexOf('}'));
             text = text.Remove(text.IndexOf('{'),1);
-            // text = Skobki1.Replace(text, " ");
-            // text = Skobki2.Replace(text, " ");
-            string[] mass = text.Split(',');
-            string[] isxod = new string[mass.Length + 50]; //В получившийся массив нужно записать отсплитованные слова
+            string[] mass = text.Split(','); //Разделяем всю строку по запятым для дальнейшего форматирования
+            string[] isxod = new string[mass.Length + 50]; //Создание массива для конечного результата. Его длина равна длине разбитого по запятым массива +50
             for (int i=0; i < mass.Length; i++)
             {
                 if (mass[i].Contains('{'))
                 {
-                    string[] skoba01 = mass[i].Split('{');
-                    string[] skobki = new string[mass.Length + 50];
+                    string[] skoba01 = mass[i].Split('{'); //Первое слово до внутренних скобок с которого могут начинатся другие окончания 
+                    string[] skobki = new string[mass.Length + 10];
                     int j = 0;
                     while (i < mass.Length)
                     {
                         i++;
-                        if (mass[i].Contains('}'))
+                        if (mass[i].Contains('}')) // Если встретился знак закрытия скобок
                         {
-                            mass[i] = mass[i].Remove(mass[i].IndexOf('}'));
-                            skobki[j] = mass[i];
+                            mass[i] = mass[i].Remove(mass[i].IndexOf('}')); //Убираем этот знак
+                            skobki[j] = mass[i]; // Записываем в массив последнее из окончаний внутрених скобок и выходим из цикла
                             break;
                         }
-                        skobki[j] = mass[i];
+                        skobki[j] = mass[i]; //Заполняем массив внутрених скобок
                         j++;
                     }
-                    isxod[o] = skoba01[0] + skoba01[1]; o++;
+                    isxod[o] = skoba01[0] + skoba01[1]; o++; //Соеденяем первое окончание с окончанием во внутрених скобках
                     for(int l = 0; skobki[l] != null; l++)
                     {
-                        isxod[o] = skoba01[0] + skobki[l]; o++;
+                        isxod[o] = skoba01[0] + skobki[l]; o++; // Соеденинение последующих окончаний
                     }
                 }
-                isxod[o] = mass[i]; o++;
+                isxod[o] = mass[i]; o++; // Записываем окончания в массив
             }
             return isxod;
         }
@@ -183,8 +183,8 @@ namespace Diplom
             Words = Sourse.Split(' ');
             Words = Words.Where(x => !string.IsNullOrWhiteSpace(x)).ToArray();
             ////////////////////////////
-            DictionarySlogs();
-            for(int i=0; i < Words.Length; i++)
+            CreateDictionary(); // Формирование словаря объектов класса word.
+            for (int i=0; i < Words.Length; i++)
             {
                 Stix.Text += Words[i] + " /// ";
                 for(int k=0; k < WordDictionary[Words[i]].slogs.Length; k++)
@@ -200,16 +200,18 @@ namespace Diplom
                 Stix.Text += "\n";
             }
             Stix.Text += "\n \n";
+
             string BY = WordBY();
             StixMetr(BY);
         }
 
         private string StixMetr(string WordBY)
         {
-            List<string> Stix = WordBY.Split(' ').ToList();
-            foreach (string s in Stix)
+            string[] Yamb = WordBY.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            for(int i=0; i < Yamb.Length - 3; i++)
             {
-                if(s.Length < 2) { Stix.Remove(s); }
+                string asd = Yamb[i] + Yamb[i + 1] + Yamb[i + 2] + Yamb[i + 3];
+                if (Yamb[i] + Yamb[i+1] + Yamb[i+2] + Yamb[i+3] == "УБУБУБУБ") { }
             }
             return "Yamb";
         }
@@ -224,8 +226,6 @@ namespace Diplom
             string WordsBY = "";
             for (int i = 0; i < Words.Length; i++)
             {
-                if (WordDictionary.ContainsKey(Words[i]))
-                {
                     //Перебор по слогам. Так как в разных словах разное кол-во то используем цикл
                     for (int j = 0; j < WordDictionary[Words[i]].slogs.Length; j++)
                     {
@@ -235,6 +235,8 @@ namespace Diplom
                             for (int k = 0; k < WordDictionary[Words[i]].slogs.Length; k++) { WordsBY += "В"; }
                             break;
                         }
+                    try
+                    {
                         if (WordDictionary[Words[i]].Accentslogs[j].Contains("'"))
                         {
                             WordsBY += "У";
@@ -245,7 +247,9 @@ namespace Diplom
                             WordsBY += "Б";
                         }
                     }
-                }
+                    catch { break; }
+                    }
+                
                 WordsBY += " ";
             }
             return WordsBY;
@@ -300,30 +304,94 @@ namespace Diplom
             return file;
         }
 
-        /////////////////////Формирование словаря слов. Слогов и удаорений ////////////////////////
-        public void DictionarySlogs()
+        /////////////////////Формирование словаря объектов класса word. Слогов и ударений ////////////////////////
+        public void CreateDictionary()
         {
-            word[] wordDic = new word[Words.Length];
+            word[] wordDic = new word[Words.Length]; //Массив объектов класса word для последующего занесения этих объектов в словарь   WordDictionary
 
             for (int i = 0; i < Words.Length; i++)
             {
                 if (!WordDictionary.ContainsKey(Words[i]))
                 {
-                    wordDic[i] = new word();
-                    wordDic[i].slogs = wordDic[i].SlogSpliter(Words[i]);
+                    wordDic[i] = new word(); 
+                    wordDic[i].slogs = wordDic[i].SlogSpliter(Words[i]); //Записываем слоги для данного слова
                     try { wordDic[i].Accent = WordAccentDictionary[Words[i]]; } //Находит ударение по ключу....По слову
                     //Если такого ключа нет  проверяем на окончания. И опять ищем по ключу
                     catch
                     {
                         ////Работа с окончаниями
-                        string abWord = AddAbout(Words[i],WordAccentDictionary);
-                        try { wordDic[i].Accent = WordAccentDictionary[abWord]; }
-                        catch { WordAccentDictionary.Add(Words[i], ""); wordDic[i].Accent = ""; } //Добавление ЭТОГО же слова как слова с ударением т.к. некоторые слова безударные
+                        string newAccent = DelOneEnd(Words[i],WordAccentDictionary); //Сначало удаляем 1 букву в конце. (самых == самый)
+                        if (!WordAccentDictionary.ContainsKey(newAccent))
+                        {
+                            newAccent = AddAbout(Words[i], WordAccentDictionary); //Удаляем по окончанию
+                            if (!WordAccentDictionary.ContainsKey(newAccent))
+                            {
+                                for (int k = 1; k < Words[i].Length / 2 - 1; k++) //Удаляем несколько букв в конце (k)
+                                {
+                                    newAccent = DelEndAddAbout(Words[i], WordAccentDictionary, k);
+                                    if (WordAccentDictionary.ContainsKey(newAccent))
+                                    {
+                                        break;
+                                    }
+                                }
+                            }
+                            if (!WordAccentDictionary.ContainsKey(newAccent))
+                            {
+                                WordAccentDictionary.Add(Words[i], ""); wordDic[i].Accent = ""; //Добавление ЭТОГО же слова как слова с ударением т.к. некоторые слова безударные
+                            }
+                        }
+                        wordDic[i].Accent = WordAccentDictionary[newAccent];
                     }
                     wordDic[i].Accentslogs = wordDic[i].SlogSpliter(wordDic[i].Accent);
                     WordDictionary.Add(Words[i], wordDic[i]);
+                   
                 }
             }
+        }
+
+        private string DelOneEnd(string word, Dictionary<string, string> AccentDictionary)//Удаление одной буквы в слове и замена на другую
+        {
+            string NewWord = "";
+            string[] ABC = {"а","б","в","г","д","е","ё","ж","з","и","й","к","л","м","н","о","п","р","с","т","у","ф","х","ц","ч","ш","щ","ъ","ы","ь","э","ю","я"};
+            NewWord = word.Remove(word.Length - 1);
+            if (!AccentDictionary.ContainsKey(NewWord))
+            {
+                string s = NewWord;
+                for (int i=0; i< ABC.Length; i++)
+                {
+                    NewWord = s + ABC[i];
+                    if (AccentDictionary.ContainsKey(NewWord)) { return NewWord; }
+                }
+            }
+            if (AccentDictionary.ContainsKey(NewWord)) { return NewWord; }
+            return word;
+        }
+
+
+        private string DelEndAddAbout(string word, Dictionary<string, string> AccentDictionary, int lenght=1)//Удаление нескольких букв в слове (от 1 до lenght) и проверка по морфологическому словарю
+        {
+            string NewWord = "";
+            NewWord = word.Remove(word.Length - lenght);
+            if (!AccentDictionary.ContainsKey(NewWord))
+            {
+                for (int j = 0; j < lexgroup.Length; j++)
+                {
+                    for (int k = 0; k < lexgroup[j].MassWords.Length; k++)
+                    {
+                        if (NewWord == lexgroup[j].MassWords[k])
+                        {
+                            string s = NewWord;
+                            for (int l = 0; l < lexgroup[j].ends.Length; l++)
+                            {
+                                NewWord = s + lexgroup[j].ends[l];
+                                if (AccentDictionary.ContainsKey(NewWord)) { return NewWord; }
+                            }
+                        }
+                    }
+                }
+            }
+            if (AccentDictionary.ContainsKey(NewWord)) { return NewWord; }
+            return word;
         }
 
         /// <summary>
@@ -331,30 +399,30 @@ namespace Diplom
         /// </summary>
         /// <param name="word">Входяшее слово</param>
         /// <returns></returns>
-        public string AddAbout(string word, Dictionary<string,string> AccentDictionary)
+        public string AddAbout(string word, Dictionary<string,string> AccentDictionary)//Удаление окончаний по словарю окончаний и замена окончания на другое из морфологического словаря
         {
             string NewWord = "";
             //Массив окончаний
             string[] ThreeAbout = { "ать", "ять", "оть", "еть", "уть", "у", "ю", "ем", "ешь", "ете", "ет","ут","ют","ал","ял","ала","яла","али","яли","ул","ула","ули",
                 "а","я","о","е","ь","ы","и","ая","яя","ое","ее","ой","ые","ие","ый","йй",
                 "ам","ами","ас","aм","ax","ая","её","ей","ем","еми","емя","ex","ею","ёт","ёте","ёх","ёшь","ий","ие","им","ими","ит","ите","их","ишь","ию","м","ми",
-                "мя","ов","ого","ое","оё","ой","ом","ому","ою","ум","умя","ут","ух","ую","шь"};
+                "мя","ов","ого","ое","оё","ой","ом","ому","ою","ум","умя","ут","ух","ую","шь","ый"};
             for (int i = 0; i < ThreeAbout.Length; i++)
             {
                 if (word.EndsWith(ThreeAbout[i]))
                 {
-                    word = word.Replace(ThreeAbout[i], "");
-                    if (!AccentDictionary.ContainsKey(word))
+                    NewWord = word.Replace(ThreeAbout[i], "");
+                    if (!AccentDictionary.ContainsKey(NewWord))
                     {
                         for (int j = 0; j < lexgroup.Length; j++)
                         {
                            for(int k=0;k < lexgroup[j].MassWords.Length; k++)
                             {
-                                if(word == lexgroup[j].MassWords[k])
+                                if(NewWord == lexgroup[j].MassWords[k])
                                 {
                                     for (int l = 0; l < lexgroup[j].ends.Length; l++)
                                     {
-                                        NewWord = word + lexgroup[j].ends[l];
+                                        NewWord = NewWord + lexgroup[j].ends[l];
                                         if (AccentDictionary.ContainsKey(NewWord)) { return NewWord; }
                                     }
                                 }
@@ -364,6 +432,7 @@ namespace Diplom
                     break;
                 }
             }
+            if (AccentDictionary.ContainsKey(NewWord)) { return NewWord; }
             return word;
         }
     }
